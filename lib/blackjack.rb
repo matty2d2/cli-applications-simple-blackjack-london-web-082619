c5f0bcfb38c8211815274e7c044de0d67498d94b
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-24 10:32:52 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,9 +32,9 @@ def hit?(card_total)
   prompt_user
   get_user_input
   
-  if get_user_input == 'h'
+  if user_input == 'h'
     card_total += deal_card
-  elsif get_user_input == 's'
+  elsif user_input == 's'
     card_total
   else
     invalid_command
